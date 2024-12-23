39aeda94e131de564fe0e78f6d36a49f441b7e71
Novak Polina <polianovak57@gmail.com>
2024-12-23 20:13:25 +0300

    Version 2 23.12

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,17 +1,18 @@
 const API_KEY = "4jHOdnrilxB7uTxQ3InrJv303NJkEudtCrOhgXEYb7OgYZAs";
-const BASE_URL = "https://api.currentsapi.services/v1/latest-news";
+const BASE_URL = "https://api.currentsapi.services/v1/search";
 import { NEWS_SERVICE } from "../constants/api";
 import axios from "axios";
 
-export const getNews = async () => {
+export const getNews = async (page_number = 1, page_size = 10) => {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
         apiKey: API_KEY,
+        page_number: page_number,
+        page_size: page_size,
       },
     });
 
-    // console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
